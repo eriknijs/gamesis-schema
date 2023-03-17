d11db6240aa8bef93c2fc509fc19cd06340685b4
Erik Nijs <eriknijs@apprigger.com>
2023-03-17 17:30:00 +0100

    fixing schema handling

---
+++ src/test/java/gamesis/story/api/ApiUtil.java
@@ -0,0 +1,29 @@
+package gamesis.story.api;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+
+
+import gamesis.auth.api.*;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import org.springframework.web.context.request.NativeWebRequest;
+
+/**
+ *
+ */
+public class ApiUtil {
+    public static void setExampleResponse(NativeWebRequest req, String contentType, String example) {
+        try {
+            HttpServletResponse res = req.getNativeResponse(HttpServletResponse.class);
+            res.setCharacterEncoding("UTF-8");
+            res.addHeader("Content-Type", contentType);
+            res.getWriter().print(example);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
